a04d34cea2fdfdf27686f0edad7bcf233d371790
Mustapha Alaba <alabamustapha@gmail.com>
2021-12-12 00:06:36 +0100

    fix: Fix wrong end_index issue
    Commit 51b370 cause end_index not to be updated due to
    no new assignment made inside the while loop

--- bikeshare.py
+++ bikeshare.py
@@ -262,11 +262,14 @@ def display_raw_data(df, start_index=0, chunk_size=5):
     """Displays raw data in chunks."""
 
     show_raw_data = input('\nWould you like see raw data? Enter yes or no.\n')
+    
     end_index = start_index + chunk_size
+    
     while show_raw_data.lower() == 'yes' and end_index < df.shape[0]:
-        print(df[start_index:start_index+chunk_size])
+        print(df[start_index:end_index])
         show_raw_data = input('\nWould you like see raw data? Enter yes or no.\n')
         start_index += chunk_size
+        end_index = start_index + chunk_size
         
 def main():
 
