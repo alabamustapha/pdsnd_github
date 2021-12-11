51b370c21d3b2e847fbb47f67ee0953ae8fc76dd
Mustapha Alaba <alabamustapha@gmail.com>
2021-12-12 00:01:35 +0100

    refactor: Updated display_raw_data function
    Restructured the conditional statement to be more readable

--- bikeshare.py
+++ bikeshare.py
@@ -262,8 +262,8 @@ def display_raw_data(df, start_index=0, chunk_size=5):
     """Displays raw data in chunks."""
 
     show_raw_data = input('\nWould you like see raw data? Enter yes or no.\n')
-    
-    while show_raw_data.lower() == 'yes' and (start_index + chunk_size) < df.shape[0]:
+    end_index = start_index + chunk_size
+    while show_raw_data.lower() == 'yes' and end_index < df.shape[0]:
         print(df[start_index:start_index+chunk_size])
         show_raw_data = input('\nWould you like see raw data? Enter yes or no.\n')
         start_index += chunk_size
